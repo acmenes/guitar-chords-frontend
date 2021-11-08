aae938cdbe7eb75d500b375b21ce75f47e416757
acmenes <acmenescompositions@gmail.com>
2021-11-08 13:13:08 -0500

    trying to write some routes tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Home from './Home';
+import UserProfile from './profiles/UserProfile';
+import LogIn from './userforms/LogIn';
+import SignUp from './userforms/SignUp';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders home link', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders signup link', () => {
+  render(<SignUp />);
+  const linkElement = screen.getByText(/signup/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders login link', () => {
+  render(<LogIn />);
+  const linkElement = screen.getByText(/login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders profile link', () => {
+  render(<UserProfile />);
+  const linkElement = screen.getByText(/profile/i);
   expect(linkElement).toBeInTheDocument();
 });
